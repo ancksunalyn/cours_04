aa85e16c28dce3c4415375b44ea50bd0e7603744
eleves <eleves@mac204-03.local>
2017-09-05 19:07:59 -0400

    version 1

--- traducteur/ViewController.swift
+++ traducteur/ViewController.swift
@@ -1,25 +1,51 @@
 //
-//  ViewController.swift
-//  traducteur
-//
-//  Created by eleves on 17-09-05.
-//  Copyright © 2017 eleves. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+{
+
+    @IBOutlet weak var anglais: UITextField!
+    @IBOutlet weak var resultat: UILabel!
+
+    let arrayAnglais = ["dog", "cat", "mouse"]
+    let arrayFrancais = ["chien", "chat", "souris"]
+    
+    @IBAction func traduction(_ sender: UIButton)
+    {
+        verification(nomDuChamp: anglais)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func verification(nomDuChamp: UITextField)
+    {
+        let c = nomDuChamp
+        if c.text != ""
+        {
+            faireTraduction (nomDuChamp: anglais, nomDuLabel: resultat)
+            print("Test")
+        }
     }
 
-
+    func faireTraduction(nomDuChamp: UITextField, nomDuLabel: UILabel)
+    {
+        let c = nomDuChamp
+        let l = nomDuLabel
+            
+        var found = false
+            
+        for index in 0..<arrayAnglais.count
+        {
+            if c.text == arrayAnglais[index]
+            {
+                l.text = arrayFrancais[index]
+                found = true
+                break;
+            }
+        }
+        if !found
+        {
+            l.text = "Pas trouvé"
+        }
+    }
 }
-
